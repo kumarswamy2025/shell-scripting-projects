c579aa200acb7ab66fbe501eeaa77093e712ec80
kumar swamy <yarramneedikumarswamy@gmail.com>
2025-03-21 18:44:16 +0530

    comments added in the monitoring free RAM space project

--- project-1/monitoring free RAM  space.sh
+++ project-1/monitoring free RAM  space.sh
@@ -1,6 +1,6 @@
-free_space=$(free -mt | grep "Total" | awk '{print $4}')
-min_ram=500
-if [[ $free_space -lt $min_ram ]]
+free_space=$(free -mt | grep "Total" | awk '{print $4}')  #This command is used to display the free RAM memory 
+min_ram=500   # min RAM value
+if [[ $free_space -lt $min_ram ]]  # Comparing  the  free_space and min_ram 
 then 
  echo "WARNING, Ram is running low"
 else
